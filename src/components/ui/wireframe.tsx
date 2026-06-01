289921a11c081c6cf137e509142f04fad90ddc52
Diego Ramos <54applets_hogs@icloud.com>
2026-06-01 12:19:22 -0400

    use new registry

--- src/components/ui/wireframe.tsx
+++ src/components/ui/wireframe.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import type { ClassValue } from "clsx";
 import { createContext, useContext, useSyncExternalStore } from "react";
+import { cn } from "@/lib/utils";
 
 const defaults = {
 	mobileBreakpoint: 768,
